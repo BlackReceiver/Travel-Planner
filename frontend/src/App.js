47b8180b0103e82c487868bd0f492605fc1b67e0
Yell Yint <yealyint1879@gmail.com>
2024-02-06 15:17:59 +0630

    Unregistered User Homepage Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Unhome from './Unhome';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is a Travel Planner headings</h1>
-      <h3>First Change</h3>
-      <h2>Second Change</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Unhome/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Unhome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Unhome() {
+  return (
+    <div>
+        <h3>This will be homepage for Unregistered Users</h3>
+    </div>
+  )
+}
+
+export default Unhome;
