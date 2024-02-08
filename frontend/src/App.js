4e3227bdf6a1ae0729f5abf0a503334e672daa21
Yell Yint <yealyint1879@gmail.com>
2024-02-08 18:12:40 +0630

    import register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Unhome from './Unhome';
-import Register from '/.Register';
+import Register from './Register';
 
 function App() {
   return (
