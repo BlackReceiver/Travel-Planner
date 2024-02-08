9473839e7f44201f8b1c987943ed5f48f06c211f
Yell Yint <yealyint1879@gmail.com>
2024-02-08 11:09:18 +0630

    Register.js created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Unhome from './Unhome';
+import Register from '/.Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Unhome/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
       </Routes>
     </BrowserRouter>
   );
