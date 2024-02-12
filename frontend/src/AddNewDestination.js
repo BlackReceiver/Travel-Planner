8abcb32f0b8d571d96e80568def63f924d2c5bec
Yell Yint <yealyint1879@gmail.com>
2024-02-12 21:54:48 +0630

    Images folder created

--- frontend/src/AddNewDestination.js
+++ frontend/src/AddNewDestination.js
@@ -1,21 +1,48 @@
-import React from 'react'
+import React from 'react';
+import Logo from './Images/IATLogo.png';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
 
 function AddNewDestination() {
   return (
     <div>
-        <ul className='nav nav-fill'>
-            <li className='nav-item'>
-                <a href='#' className='nav-link'><img src='..\..\Necessary Images\IAT-Logo-Long.png'/></a>
-            </li>
+        <nav className='navbar navbar-expand-sm navbar-dark bg-light'>
+            <div className='container-fluid'>
+            <a href='#' className='nav-link'><img src={Logo} alt='Logo' className='img-fluid' style={{ maxWidth: '150px', height: 'auto' }}/></a>
 
-            <li className='nav-item'>
-                <a href='' className='nav-link'>Manage Accounts</a>
-            </li>
+            <ul className='navbar-nav'>
+                <li className='nav-item'>
+                    <a href='#' className='nav-link active'>Manage Accounts</a>
+                </li>
 
-            <li className='nav-item'>
-                <a href='' className='nav-link'>Manage Destinations</a>
-            </li>
-        </ul>
+                <li className='nav-item'>
+                    <a href='#' className='nav-link'>Manage Destinations</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+
+
+
+
+
+
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="#home"><img src={Logo} alt='Logo' className='img-fluid' style={{ maxWidth: '150px', height: 'auto' }}/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="#">Manage Accounts</Nav.Link>
+                    <Nav.Link href="#">Manage Destinations</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+
+
+
+
+        
     </div>
   )
 }
