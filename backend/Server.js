cb65d61821f10dc670f177fc7a80a052057aafb7
Yell Yint <yealyint1879@gmail.com>
2024-02-12 20:53:46 +0630

    npm install cookie-parser,jsonwebtoken and accountmanagement js file created

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,17 @@
-const express = require('express');
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+
 
 const app = express();
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "travel_planner"
+})
 
 app.listen(8081, () => {
     console.log("Listening");
