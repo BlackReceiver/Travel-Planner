60e7a7d3645901b9847722c4548f40c086657123
Yell Yint <yealyint1879@gmail.com>
2024-02-14 00:21:58 +0630

    App.js modify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,10 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Unhome from './Unhome';
 import Register from './Register';
-<<<<<<< HEAD
+
 import ManageAccoount from './ManageAccount';
-=======
-import AccountManagement from './AccountManagement';
 import AddNewDestination from './AddNewDestination';
->>>>>>> 92dfe0c5bc4806f8e44790861ce8c83249d34fa5
+
 
 function App() {
   return (
@@ -15,12 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Unhome/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
-<<<<<<< HEAD
         <Route path='/manageAccount' element={<ManageAccoount/>}></Route>
-=======
-        <Route path='accountmanagement' element={<AccountManagement/>}></Route>
         <Route path='/addnewdestination' element={<AddNewDestination/>}></Route>
->>>>>>> 92dfe0c5bc4806f8e44790861ce8c83249d34fa5
       </Routes>
     </BrowserRouter>
   );
